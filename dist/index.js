2c9228cf6894616942f5a965d1571f09f2aaac76
yangning <ning@edison.tech>
2021-08-02 17:03:01 +0800

    init

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EN = void 0;
-var en_1 = __importDefault(require("./lib/en"));
-exports.EN = en_1.default;
--- index.ts
+++
@@ -1,2 +0,0 @@
-import en from "./lib/en";
-export const EN = en;
